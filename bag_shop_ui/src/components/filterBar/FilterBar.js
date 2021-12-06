8f83200ce77e6d1973df7a6ae541531448fce0a9
hrihoryeu <egoragrig@gmail.com>
2021-12-06 13:24:21 +0300

    feat: big commit2

--- bag_shop_ui/src/components/filterBar/FilterBar.js
+++ bag_shop_ui/src/components/filterBar/FilterBar.js
@@ -6,7 +6,17 @@ import BagService from "../../services/BagService";
 export default class FilterBar extends Component {
     state = {
         brandToggle: false,
-        brandList: []
+        brandList: [],
+        filtersList: [],
+
+        filterList: [
+            'all',
+            'manufacturer',
+            'color',
+            'material',
+            'form',
+            'size'
+        ]
     }
 
     bagService = new BagService();
@@ -20,6 +30,12 @@ export default class FilterBar extends Component {
     onBrandListLoaded = (brandList) => {
         this.setState({
             brandList
+        })
+    }
+
+    onFilterLoaded = (filtersList) => {
+        this.setState({
+            filtersList
         })
     }
 
@@ -37,6 +53,14 @@ export default class FilterBar extends Component {
         return brandList
     }
 
+    renderFilters(arr) {
+        const filtersList = arr.map((filterName) => {
+            this.bagService
+                .getAlllItems(filterName)
+                .then(this.onFilterLoaded)
+        });
+    }
+
     onBrandToggle = () => {
         this.setState((state) => {
             return {
@@ -45,12 +69,10 @@ export default class FilterBar extends Component {
         })
     }
 
-    itemSelected = (name) => {
-        console.log(name)
-    }
-
     render() {
-        const { brandList, brandToggle } = this.state;
+        const { brandList, brandToggle, filtersList, filterList } = this.state;
+
+        console.log(filtersList);
 
         const brandFilter = brandToggle ? this.renderBrands(brandList) : 'BRANDS';
         const brandClassName = brandToggle ? "filter_bar__select__flex" : "filter_bar__select"
@@ -58,7 +80,8 @@ export default class FilterBar extends Component {
         return (
             <div className='filter_bar'>
                 <div
-                    className="filter_bar__head">
+                    className="filter_bar__head"
+                    onClick={() => this.renderFilters(filterList)}>
                     FILTERS
                 </div>
                 <div
--- bag_shop_ui/src/services/BagService.js
+++ bag_shop_ui/src/services/BagService.js
@@ -1,6 +1,11 @@
+import * as url from "url";
+
 export default class BagService {
+    _apiBase = 'http://127.0.0.1:8000/api/'
     _apiBaseProduct = 'http://127.0.0.1:8000/api/product/'
     _apiBaseManufacturer = 'http://127.0.0.1:8000/api/manufacturer/'
+
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -14,6 +19,15 @@ export default class BagService {
     getAllItems = async () => {
         const res = await this.getResource(`${this._apiBaseProduct}`);
         return res.map(this._transformItem);
+    }
+
+    getAlllItems = async (filterName) => {
+        let url = '';
+        filterName === 'all' ? url = `${this._apiBase}product/` : url = `${this._apiBase}${filterName}/`
+        const res = await this.getResource(url);
+        const result = {filterName: filterName,
+                        data: res.map(this.mapping[filterName])}
+        return result;
     }
 
     getManufacturerItems = async (manufacturer) => {
@@ -42,7 +56,51 @@ export default class BagService {
             product_name: item.product_name,
             description: item.description,
             manufacturer: item.manufacturer,
-            thumbnail: item.thumbnail ? item.thumbnail.slice(21) : "/media/product_img/no_image.png"
+            thumbnail: item.thumbnail ? item.thumbnail.slice(21) : "/media/product_img/no_image.png",
         }
+    }
+
+    _transformManufacturer = (item) => {
+        return {
+            id: item.id,
+            manufacturer_name: item.manufacturer_name,
+        }
+    }
+
+    _transformColor = (item) => {
+        return {
+            id: item.id,
+            color_name: item.color_name,
+        }
+    }
+
+    _transformMaterial = (item) => {
+        return {
+            id: item.id,
+            material_name: item.material_name,
+        }
+    }
+
+    _transformForm = (item) => {
+        return {
+            id: item.id,
+            form_name: item.form_name,
+        }
+    }
+
+    _transformSize = (item) => {
+        return {
+            id: item.id,
+            size_name: item.size_name
+        }
+    }
+
+    mapping = {
+        'all': this._transformItem,
+        'manufacturer': this._transformManufacturer,
+        'color': this._transformColor,
+        'material': this._transformMaterial,
+        'form': this._transformForm,
+        'size': this._transformSize,
     }
 }
